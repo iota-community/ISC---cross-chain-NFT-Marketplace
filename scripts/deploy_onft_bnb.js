448eaf3aa0326e2e5abf41a88001e94ced470505
salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
2024-08-14 13:27:16 +0200

    feat: deploy onft
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>

--- scripts/deploy_onft_bnb.js
+++ scripts/deploy_onft_bnb.js
@@ -3,17 +3,17 @@ const path = require('path');
 const { ethers } = require('hardhat');
 
 async function deployMyONFT721(name, symbol, minGasToTransfer,lzEndpointAddress) {
-    const MyProxyONFT721 = await ethers.getContractFactory("MyONFT721");
-    const myProxyONFT721 = await MyProxyONFT721.deploy(name, symbol, minGasToTransfer,lzEndpointAddress);
+    const MyONFT = await ethers.getContractFactory("MyONFT721");
+    const myONFT = await MyONFT.deploy(name, symbol, minGasToTransfer,lzEndpointAddress);
 
-    const address = await myProxyONFT721.getAddress();
+    const address = await myONFT.getAddress();
     console.log("MyONFT721 deployed to:", address);
     return address;
 }
 
 async function main() {
-    const minGasToTransfer = 100000; // Example value, adjust based on your needs
-    const lzEndpointAddress = "0xae92d5aD7583AD66E49A0c67BAd18F6ba52dDDc1"; 
+    const minGasToTransfer = 100000;
+    const lzEndpointAddress = "0x6Fcb97553D41516Cb228ac03FdC8B9a0a9df04A1"; // Endpoint on Binance Smart Chain
 
     const name = "SampleToken";
     const symbol = "SESA";
@@ -24,11 +24,13 @@ async function main() {
     const addressDirectory = path.join(__dirname, 'addresses');
     fs.mkdirSync(addressDirectory, { recursive: true });
 
-    const filePath = path.join(addressDirectory, 'MyONFT721_Shimmer.txt');
+    const filePath = path.join(addressDirectory, 'MyONFT721_Bnb.txt');
     fs.writeFileSync(filePath, deployedAddress);
 
     console.log(`Contract address written to ${filePath}`);
 }
+
+
 
 main()
     .then(() => process.exit(0))
